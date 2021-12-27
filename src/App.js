5e31fb877dd313fbedf619c9c90288827e6f1362
Rajat Kumar <rajatk12199@gmail.com>
2021-12-27 15:43:07 +1100

    revert state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h2 id="title">bar chart</h2>
         <body />
       </div>
